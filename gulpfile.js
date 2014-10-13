f0d866ac0a858c40cd69fadac7c3447dd6c3cf50
Andrea Moretti <axyzxp@gmail.com>
2014-10-13 23:07:39 +0200

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,34 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var path = require('path');
+var changed = require('gulp-changed');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 var browserify = require('browserify');
+var sass = require('gulp-sass');
+var config = require(path.join(__dirname, 'config.json'));
 
-/**
- * OPTIONS
- */
-var opts = {
-  src: 'src',
-  dist: 'dist',
-  htmlDir: 'html',
-  imgDir: 'img',
-  cssiDir: 'css',
-  jsonDir: 'json',
-  fontDir: 'font',
-  browserifyIndexFile: 'index.jsx',
-
-  // optionals
-  reactify: true,
-  sass: true,
-  sassDir: 'sass',
-  sourcemaps: true
-};
+var opts = config.opts;
 
 if(opts.reactify)
   var reactify = require('reactify');
-if(opts.sass)
-  var sass = require('gulp-sass');
 if(opts.sourcemaps)
   var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function() {
+  var bundler = browserify(path.join(__dirname, opts.src, opts.browserifyIndexFile))
+
+  if(opts.reactify)
+    bundler.transform('reactify');
+
+  return bundler
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(path.join(__dirname, opts.dist)));
+});
+
+gulp.task('watch-scripts', function() {
   var bundler = watchify(browserify(path.join(__dirname, opts.src, opts.browserifyIndexFile), watchify.args));
 
   if(opts.reactify)
@@ -46,40 +41,48 @@ gulp.task('scripts', function() {
     return bundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest(path.join(__dirname, opts.dist)));
+    .pipe(gulp.dest(path.join(__dirname, opts.dist)))
+    .pipe(reload({ stream: true }));
   }
 
   return rebundle();
 });
 
 gulp.task('styles', function() {
-  gulp.src(path.join(__dirname, opts.src, opts.cssDir, '**', '*.css'))
-    .pipe(gulp.dest(path.join(__dirname, opts.dist, opts.cssDir)));
-
-  if(sass) {
-    if(!sourcemaps) {
-      gulp.src(path.join(__dirname, opts.src, opts.sassDir, '*.scss'))
+  if(!sourcemaps) {
+    gulp.src(path.join(__dirname, opts.src, opts.sassDir, '*.scss'))
+      .pipe(sass())
+      .pipe(gulp.dest(path.join(__dirname, opts.dist, opts.cssDir)))
+      .pipe(reload({ stream: true }));
+  }else {
+    gulp.src(path.join(__dirname, opts.src, opts.sassDir, '*.scss'))
+      .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(gulp.dest(path.join(__dirname, opts.dist, opts.cssDir)));
-    }else {
-      gulp.src(path.join(__dirname, opts.src, opts.sassDir, '*.scss'))
-        .pipe(sourcemaps.init())
-          .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(path.join(__dirname, opts.dist, opts.cssDir));
-    }
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.join(__dirname, opts.dist, opts.cssDir)))
+      .pipe(reload({ stream: true }));
   }
 });
 
 gulp.task('html', function() {
   gulp.src(path.join(__dirname, opts.src, opts.htmlDir, '**', '*.html'))
+    .pipe(changed(path.join(__dirname, opts.dist)))
     .pipe(gulp.dest(path.join(__dirname, opts.dist)));
 });
 
-gulp.task('serve', function() {
+gulp.task('browser-sync', ['build'], function() {
   browserSync({
     server: {
       baseDir: path.join(__dirname, opts.dist)
     }
   });
 });
+
+gulp.task('watch', ['build', 'browser-sync'], function() {
+  gulp.watch(path.join(__dirname, opts.src, opts.htmlDir, '**', '*.html'), ['html', reload]);
+  gulp.watch(path.join(__dirname, opts.src, opts.sassDir, '**', '*.scss'), ['styles']);
+  gulp.watch(path.join(__dirname, opts.src, '**', '*.jsx'), ['watch-scripts']);
+});
+
+gulp.task('build', ['scripts', 'styles', 'html']);
+gulp.task('default', ['watch']);
